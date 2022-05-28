611686fc2bb9c803c8d08914035d3b98fd505a26
Dev5ter <hootiemunsey@yahoo.com>
2022-05-28 17:17:55 -0400

    Not Supported Checks = Red Rupee

--- not_supported.py
+++ not_supported.py
@@ -1,6 +1,41 @@
-
 locations_not_supported = [
+    "Windfall Island - Lenzo's House - Become Lenzo's Assistant",
+    "Windfall Island - Lenzo's House - Bring Forest Firefly",
+    "Windfall Island - 5 Rupee Auction",
+    "Windfall Island - 40 Rupee Auction",
+    "Windfall Island - 60 Rupee Auction",
+    "Windfall Island - 80 Rupee Auction",
+    "Windfall Island - Battlesquid - Under 20 Shots Prize",
+    "Windfall Island - Maggie - Free Item",
+    "Windfall Island - Maggie - Delivery Reward",
+    "Windfall Island - Mila - Follow the Thief",
+    "Windfall Island - Pompie and Vera - Secret Meeting Photo",
+    "Windfall Island - Minenco - Miss Windfall Photo",
+    "Windfall Island - Ivan - Catch Killer Bees",
     "Windfall Island - Mrs. Marie - Catch Killer Bees",
+    "Windfall Island - Mrs. Marie - Give 1 Joy Pendant",
+    "Windfall Island - Mrs. Marie - Give 21 Joy Pendants",
+    "Windfall Island - Mrs. Marie - Give 40 Joy Pendants",
+    "Windfall Island - Cafe Bar - Postman",
+    "Windfall Island - Tott - Teach Rhythm",
+    "Windfall Island - Zunari - Stock Exotic Flower in Zunari's Shop",
+    "Windfall Island - Sam - Decorate the Town",
+    "Windfall Island - Maggie's Father - Give 20 Skull Necklaces",
+    "Windfall Island - Linda and Anton",
+    "Windfall Island - Kamo - Full Moon Photo",
+
+    "Outset Island - Great Fairy",
+    "Outset Island - Orca - Hit 500 Times",
+
+    "Thorned Fairy Island - Great Fairy",
+    "Eastern Fairy Island - Great Fairy",
+    "Western Fairy Island - Great Fairy",
+    "Southern Fairy Island - Great Fairy",
+    "Northern Fairy Island - Great Fairy",
+
+    "Tingle Island - Ankle - Reward for All Tingle Statues",
+
+    "Flight Control Platform - Bird-Man Contest - First Prize",
 
     "Mailbox - Letter from Hoskit's Girlfriend",
     "Mailbox - Letter from Baito's Mother",
@@ -15,5 +50,12 @@ locations_not_supported = [
     "Mailbox - Letter from Aryll",
     "Mailbox - Letter from Tingle",
     
-    "The Great Sea - Beedle's Shop Ship - 20 Rupee Item"
+    "Dragon Roost Island - Rito Aerie - Give Hoskit 20 Golden Feathers",
+    
+    "Spectacle Island - Barrel Shooting - First Prize",
+    "Spectacle Island - Barrel Shooting - Second Prize",
+
+    "The Great Sea - Beedle's Shop Ship - 20 Rupee Item",
+    "The Great Sea - Salvage Corp Gift",
+    "The Great Sea - Goron Trading Reward"
 ]
